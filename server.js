fa5d21fe5af64498d870a444b302a55db9ee0451
NguyenPhuc <20521767@gm.uit.edu.vn>
2023-03-07 16:02:33 +0700

    basic change

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDb = require("./config/db");
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger");
+const errorHandler = require("./middleware/error");
 
 const app = express();
 connectDb();
@@ -9,17 +11,19 @@ connectDb();
 app.use(express.json());
 
 const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: process.env.PROJECT_NAME,
-            description: process.env.PROJECT_NAME + " API"
-        },
-    },
-    apis: ["app.js", "./routes/*.js", "./model/*/*.js"]
+    swaggerDefinition: swaggerDocument,
+    apis: ["server.js", "./routes/*.js", "./middleware/*.js", "./models/*.js"]
 }
+
+app.get("/", function (req, res) {
+	res.redirect("/doc");
+});
 
 app.use("/api/catchphrases", require("./routes/catchphrases"));
 
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerJsDoc(swaggerOptions)));
+
+// Error Handler
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 5000, () => console.log("Up and running 🚀"));
