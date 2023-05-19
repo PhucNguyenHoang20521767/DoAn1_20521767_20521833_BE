a60de6e6e12622c6c78f2045600d56bfb704591e
NguyenPhuc <20521767@gm.uit.edu.vn>
2023-05-19 20:27:34 +0700

    add cors again & add change password for customer

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use("/", (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
 passport.serializeUser((user, done) => {
 	done(null, user);
 });
