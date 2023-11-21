16a5f66166131a4362c9848a9c931c79af38f109
PhucNguyenHoang <hoangphucnguyen112002@gmail.com>
2023-11-21 13:44:42 +0700

    NguyenPhuc - Change logic of blog posts management

--- controllers/blogPostController.js
+++ controllers/blogPostController.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 const ErrorResponse = require("../utils/errorResponse");
 
 const BlogPost = require("../models/blog_post/blog_post");
-const PostContent = require("../models/blog_post/post_content");
-const ContentImage = require("../models/blog_post/content_image");
 
 const firebaseStorage = require("../config/firebase");
 const { ref, getDownloadURL } = require("firebase/storage");
@@ -11,24 +9,36 @@ const { ref, getDownloadURL } = require("firebase/storage");
 // Blog Post
 exports.getAllBlogPosts = async (req, res, next) => {
     let options = {};
-    
+
+    if (req.search) {
+        options = {
+            ...options,
+            $or: [
+                { blogPostTitle: new RegExp(req.search.toString(), "i") }
+            ],
+        }
+    }
+
     let total = BlogPost.countDocuments(options);
-    let page = parseInt(req.query.page) || 1;
-    let limit = parseInt(req.query.limit) || parseInt(await total);
-    let last_page = Math.ceil(parseInt(await total)/limit);
-    if (last_page < 1 && total > 0) {
-        last_page = 1
+    let lastPage = Math.ceil(parseInt(await total) / req.pagination.limit);
+    
+    if (lastPage < 1 && total > 0) {
+        lastPage = 1
     }
 
     try {
-        const blogPosts = await BlogPost.find(options);
+        const blogPosts = await BlogPost
+            .find(options)
+            .limit(req.pagination.limit)
+            .skip(req.pagination.skip);
+
         res.status(200).json({
             success: true,
             message: "List of blog posts fetched successfully",
             data: blogPosts,
             total: (await total).toString(),
-            page: (await page).toString(),
-            last_page: (await last_page).toString()
+            page: (await req.pagination.page).toString(),
+            lastPage: (await lastPage).toString()
         });
     } catch (error) {
         next(error);
@@ -57,18 +67,20 @@ exports.getBlogPostById = async (req, res, next) => {
 };
 
 exports.createBlogPost = async (req, res, next) => {
-    const { blogPostTitle, blogPostTag, blogPostDescription } = req.body;
+    const { blogPostTitle, blogPostAuthor, blogPostTag, blogPostDescription, blogPostContent } = req.body;
 
     try {
         await BlogPost.create({
             blogPostTitle,
+            blogPostAuthor,
             blogPostTag,
-            blogPostDescription
+            blogPostDescription,
+            blogPostContent
         });
 
         res.status(201).json({
             success: true,
-            message: "Blog post created successfully"
+            message: "Create blog post successfully"
         });
     } catch (error) {
         next(error);
@@ -88,7 +100,7 @@ exports.saveBlogPostThumbnail = async (req, res, next) => {
         if (!blogPost)
             return next(new ErrorResponse("No blog post found", 404));
 
-        const thumbnailURL = await getDownloadURL(ref(firebaseStorage, `attachments/${req.thumbnailOriginalName}`))
+        const thumbnailURL = await getDownloadURL(ref(firebaseStorage, `attachments/${req.thumbnailOriginalName}`));
 
         await blogPost.updateOne({
             blogPostThumbnail: thumbnailURL
@@ -99,6 +111,49 @@ exports.saveBlogPostThumbnail = async (req, res, next) => {
         res.status(200).json({
             success: true,
             message: "Blog post's thumbnail saved successfully"
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.uploadBlogPostImage = async (req, res, next) => {
+    try {
+        const imageURL = await getDownloadURL(ref(firebaseStorage, `attachments/${req.imageOriginalName}`));
+
+        res.status(200).json({
+            success: true,
+            message: "Upload blog post's image successfully",
+            imageURL: imageURL
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.updateBlogPost = async (req, res, next) => {
+    const { blogPostId } = req.params;
+
+    if (!blogPostId || !mongoose.Types.ObjectId.isValid(blogPostId)) {
+        return next(new ErrorResponse("Please provide valid blog post's ID", 400));
+    }
+    
+    const { blogPostTitle, blogPostTag, blogPostDescription, blogPostContent } = req.body;
+
+    try {
+        const blogPost = await BlogPost.findByIdAndUpdate(blogPostId, {
+            blogPostTitle,
+            blogPostTag,
+            blogPostDescription,
+            blogPostContent
+        });
+
+        if (!blogPost)
+            return next(new ErrorResponse("No blog post found", 404));
+
+        res.status(200).json({
+            success: true,
+            message: "Update blog post successfully"
         });
     } catch (error) {
         next(error);
@@ -132,81 +187,105 @@ exports.hideOrUnhideBlogPost = async (req, res, next) => {
     }
 };
 
-// Post Content
-exports.addBlogPostContent = async (req, res, next) => {
+exports.deleteBlogPost = async (req, res, next) => {
     const { blogPostId } = req.params;
 
     if (!blogPostId || !mongoose.Types.ObjectId.isValid(blogPostId)) {
         return next(new ErrorResponse("Please provide valid blog post's ID", 400));
     }
-
-    const { partTitle, partContent } = req.body;
-
+    
     try {
-        const blogPost = await BlogPost.findById(blogPostId);
+        const blogPost = await BlogPost.findByIdAndDelete(blogPostId);
 
         if (!blogPost)
             return next(new ErrorResponse("No blog post found", 404));
 
-        const postContent = new PostContent({
-            partTitle,
-            partContent
-        });
-
-        blogPost.blogPostContent.push(postContent);
-
-        await blogPost.save();
-
         res.status(200).json({
             success: true,
-            message: "Add blog post content successfully",
-            postContentId: postContent._id
+            message: "Delete blog post successfully"
         });
     } catch (error) {
         next(error);
     }
 };
 
-
-// Content Image
-exports.addPostContentImage = async (req, res, next) => {
-    const { blogPostId } = req.params;
-
-    if (!blogPostId || !mongoose.Types.ObjectId.isValid(blogPostId)) {
-        return next(new ErrorResponse("Please provide valid blog post's ID", 400));
-    }
-
-    const { postContentId, contentImageDescription } = req.body;
-
-    if (!postContentId || !mongoose.Types.ObjectId.isValid(postContentId)) {
-        return next(new ErrorResponse("Please provide valid post content's ID", 400));
-    }
-
-    try {
-        const blogPost = await BlogPost.findById(blogPostId);
-
-        if (!blogPost)
-            return next(new ErrorResponse("No blog post found", 404));
-        
-        const imageURL = await getDownloadURL(ref(firebaseStorage, `attachments/${req.contentImageOriginalName}`))
-
-        const contentImage = new ContentImage({
-            contentImage: imageURL,
-            contentImageDescription
-        });
-
-        blogPost.blogPostContent.forEach((item, index) => {
-            if (item._id == postContentId)
-                blogPost.blogPostContent[index].partImage.push(contentImage);
-        });
-
-        await blogPost.save();
-
-        res.status(200).json({
-            success: true,
-            message: "Blog post content's image saved successfully"
-        });
-    } catch(error) {
-        next(error);
-    }
-};
+// const PostContent = require("../models/blog_post/post_content");
+// const ContentImage = require("../models/blog_post/content_image");
+//
+// // Post Content
+// exports.addBlogPostContent = async (req, res, next) => {
+//     const { blogPostId } = req.params;
+//
+//     if (!blogPostId || !mongoose.Types.ObjectId.isValid(blogPostId)) {
+//         return next(new ErrorResponse("Please provide valid blog post's ID", 400));
+//     }
+//
+//     const { partTitle, partContent } = req.body;
+//
+//     try {
+//         const blogPost = await BlogPost.findById(blogPostId);
+//
+//         if (!blogPost)
+//             return next(new ErrorResponse("No blog post found", 404));
+//
+//         const postContent = new PostContent({
+//             partTitle,
+//             partContent
+//         });
+//
+//         blogPost.blogPostContent.push(postContent);
+//
+//         await blogPost.save();
+//
+//         res.status(200).json({
+//             success: true,
+//             message: "Add blog post content successfully",
+//             postContentId: postContent._id
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+//
+// // Content Image
+// exports.addPostContentImage = async (req, res, next) => {
+//     const { blogPostId } = req.params;
+//
+//     if (!blogPostId || !mongoose.Types.ObjectId.isValid(blogPostId)) {
+//         return next(new ErrorResponse("Please provide valid blog post's ID", 400));
+//     }
+//
+//     const { postContentId, contentImageDescription } = req.body;
+//
+//     if (!postContentId || !mongoose.Types.ObjectId.isValid(postContentId)) {
+//         return next(new ErrorResponse("Please provide valid post content's ID", 400));
+//     }
+//
+//     try {
+//         const blogPost = await BlogPost.findById(blogPostId);
+//
+//         if (!blogPost)
+//             return next(new ErrorResponse("No blog post found", 404));
+//     
+//         const imageURL = await getDownloadURL(ref(firebaseStorage, `attachments/${req.contentImageOriginalName}`))
+//
+//         const contentImage = new ContentImage({
+//             contentImage: imageURL,
+//             contentImageDescription
+//         });
+//
+//         blogPost.blogPostContent.forEach((item, index) => {
+//             if (item._id == postContentId)
+//                 blogPost.blogPostContent[index].partImage.push(contentImage);
+//         });
+//
+//         await blogPost.save();
+//
+//         res.status(200).json({
+//             success: true,
+//             message: "Blog post content's image saved successfully"
+//         });
+//     } catch(error) {
+//         next(error);
+//     }
+// };
--- routes/blog_posts.js
+++ routes/blog_posts.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { query } = require("../middleware/query");
 const { protect, adminProtect } = require("../middleware/auth");
 const { uploadMemoryStorage } = require("../config/attachment");
 
-const { getAllBlogPosts, getBlogPostById, createBlogPost, saveBlogPostThumbnail, hideOrUnhideBlogPost,
-    addBlogPostContent,
-    addPostContentImage } = require("../controllers/blogPostController");
+const { getAllBlogPosts, getBlogPostById, createBlogPost, saveBlogPostThumbnail, uploadBlogPostImage, updateBlogPost, hideOrUnhideBlogPost, deleteBlogPost } = require("../controllers/blogPostController");
 
 const firebaseStorage = require("../config/firebase");
 const { ref, uploadBytesResumable } = require("firebase/storage");
@@ -17,13 +16,26 @@ const { ref, uploadBytesResumable } = require("firebase/storage");
  *     tags: [Blog Post]
  *     operatorId: getAllBlogPosts
  *     description: Get all blog posts
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         type: string
+ *         description: Search by blog post's title
+ *       - in: query
+ *         name: page
+ *         type: string
+ *         description: Specify page number
+ *       - in: query
+ *         name: limit
+ *         type: string
+ *         description: Limit the number of rows returned from a query
  *     responses:
  *       200:
  *         description: Success
  *       400:
  *         description: Bad Request
  */
-router.route("/getAllBlogPosts").get(getAllBlogPosts);
+router.route("/getAllBlogPosts").get(query, getAllBlogPosts);
 
 /**
  * @swagger
@@ -65,9 +77,13 @@ router.route("/getBlogPostById/:blogPostId").get(getBlogPostById);
  *             properties:
  *               blogPostTitle:
  *                 type: string
+ *               blogPostAuthor:
+ *                 type: string
  *               blogPostTag:
  *                 type: string
  *               blogPostDescription:
+ *                 type: string
+ *               blogPostContent:
  *                 type: string
  *     responses:
  *       201:
@@ -118,7 +134,7 @@ router.route("/saveBlogPostThumbnail/:blogPostId").post(adminProtect, protect, u
     try {
         if (req.files) {
             req.files.forEach((file) => {
-                file.originalname = "blog-post_" + file.originalname + "_" + Date.now();
+                file.originalname = "blogPostThumbnail_" + file.originalname + "_" + Date.now();
                 req.thumbnailOriginalName = file.originalname;
                 uploadBytesResumable(ref(firebaseStorage, `attachments/${file.originalname}`), file.buffer, { contentType: file.mimetype});
             });
@@ -126,11 +142,95 @@ router.route("/saveBlogPostThumbnail/:blogPostId").post(adminProtect, protect, u
 
         setTimeout(() => {
             next();
-        }, 5000);
+        }, 10000);
     } catch (error) {
         next(error);
     }
 }, saveBlogPostThumbnail);
+
+/**
+ * @swagger
+ * /api/posts/uploadBlogPostImage:
+ *   post:
+ *     tags: [Blog Post]
+ *     operatorId: uploadBlogPostImage
+ *     description: Upload blog post's image
+ *     security:
+ *       - bearer: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - Files[]
+ *             properties:
+ *               Files[]:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                   format: binary
+ *     responses:
+ *       200:
+ *         description: Uploaded
+ */
+router.route("/uploadBlogPostImage").post(adminProtect, protect, uploadMemoryStorage.array("Files[]"), async (req, res, next) => {
+    try {
+        if (req.files) {
+            req.files.forEach((file) => {
+                file.originalname = "blogPostImage_" + file.originalname + "_" + Date.now();
+                req.imageOriginalName = file.originalname;
+                uploadBytesResumable(ref(firebaseStorage, `attachments/${file.originalname}`), file.buffer, { contentType: file.mimetype});
+            });
+        }
+
+        setTimeout(() => {
+            next();
+        }, 10000);
+    } catch (error) {
+        next(error);
+    }
+}, uploadBlogPostImage);
+
+/**
+ * @swagger
+ * /api/posts/updateBlogPost/{id}:
+ *   put:
+ *     tags: [Blog Post]
+ *     operatorId: updateBlogPost
+ *     description: Update blog post
+ *     security:
+ *       - bearer: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         type: string
+ *         description: Blog Post ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               blogPostTitle:
+ *                 type: string
+ *               blogPostTag:
+ *                 type: string
+ *               blogPostDescription:
+ *                 type: string
+ *               blogPostContent:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       404:
+ *         description: Not Found
+ */
+router.route("/updateBlogPost/:blogPostId").put(adminProtect, protect, updateBlogPost);
 
 /**
  * @swagger
@@ -159,11 +259,11 @@ router.route("/hideOrUnhideBlogPost/:blogPostId").put(adminProtect, protect, hid
 
 /**
  * @swagger
- * /api/posts/addBlogPostContent/{id}:
- *   put:
+ * /api/posts/deleteBlogPost/{id}:
+ *   delete:
  *     tags: [Blog Post]
- *     operatorId: addBlogPostContent
- *     description: Add blog post content
+ *     operatorId: deleteBlogPost
+ *     description: Delete blog post
  *     security:
  *       - bearer: []
  *     parameters:
@@ -171,85 +271,111 @@ router.route("/hideOrUnhideBlogPost/:blogPostId").put(adminProtect, protect, hid
  *         name: id
  *         required: true
  *         type: string
- *         description: Blog Post ID
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             properties:
- *               partTitle:
- *                 type: string
- *               partContent:
- *                 type: string
+ *         description: Blog post ID
  *     responses:
  *       200:
- *         description: Added
+ *         description: Success
  *       400:
  *         description: Bad Request
  *       404:
  *         description: Not Found
  */
-router.route("/addBlogPostContent/:blogPostId").put(adminProtect, protect, addBlogPostContent);
-
-/**
- * @swagger
- * /api/posts/addPostContentImage/{id}:
- *   put:
- *     tags: [Blog Post]
- *     operatorId: addPostContentImage
- *     description: Add post content image
- *     security:
- *       - bearer: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         type: string
- *         description: Blog Post ID
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             required:
- *               - postContentId
- *               - Files[]
- *             properties:
- *               postContentId:
- *                 type: string
- *               Files[]:
- *                 type: array
- *                 items:
- *                   type: string
- *                   format: binary
- *               contentImageDescription:
- *                 type: string
- *     responses:
- *       200:
- *         description: Added
- *       400:
- *         description: Bad Request
- *       404:
- *         description: Not Found
- */
-router.route("/addPostContentImage/:blogPostId").put(adminProtect, protect, uploadMemoryStorage.array("Files[]"), async (req, res, next) => {
-    try {
-        if (req.files) {
-            req.files.forEach((file) => {
-                file.originalname = "post-content_" + file.originalname + "_" + Date.now();
-                req.contentImageOriginalName = file.originalname;
-                uploadBytesResumable(ref(firebaseStorage, `attachments/${file.originalname}`), file.buffer, { contentType: file.mimetype});
-            });
-        }
-
-        setTimeout(() => {
-            next();
-        }, 5000);
-    } catch (error) {
-        next(error);
-    }
-}, addPostContentImage);
+router.route("/deleteBlogPost/:blogPostId").delete(adminProtect, protect, deleteBlogPost);
 
 module.exports = router;
+
+// const { addBlogPostContent, addPostContentImage } = require("../controllers/blogPostController");
+// /**
+//  * @swagger
+//  * /api/posts/addBlogPostContent/{id}:
+//  *   put:
+//  *     tags: [Blog Post]
+//  *     operatorId: addBlogPostContent
+//  *     description: Add blog post content
+//  *     security:
+//  *       - bearer: []
+//  *     parameters:
+//  *       - in: path
+//  *         name: id
+//  *         required: true
+//  *         type: string
+//  *         description: Blog Post ID
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             properties:
+//  *               partTitle:
+//  *                 type: string
+//  *               partContent:
+//  *                 type: string
+//  *     responses:
+//  *       200:
+//  *         description: Added
+//  *       400:
+//  *         description: Bad Request
+//  *       404:
+//  *         description: Not Found
+//  */
+// router.route("/addBlogPostContent/:blogPostId").put(adminProtect, protect, addBlogPostContent);
+//
+// /**
+//  * @swagger
+//  * /api/posts/addPostContentImage/{id}:
+//  *   put:
+//  *     tags: [Blog Post]
+//  *     operatorId: addPostContentImage
+//  *     description: Add post content image
+//  *     security:
+//  *       - bearer: []
+//  *     parameters:
+//  *       - in: path
+//  *         name: id
+//  *         required: true
+//  *         type: string
+//  *         description: Blog Post ID
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         multipart/form-data:
+//  *           schema:
+//  *             type: object
+//  *             required:
+//  *               - postContentId
+//  *               - Files[]
+//  *             properties:
+//  *               postContentId:
+//  *                 type: string
+//  *               Files[]:
+//  *                 type: array
+//  *                 items:
+//  *                   type: string
+//  *                   format: binary
+//  *               contentImageDescription:
+//  *                 type: string
+//  *     responses:
+//  *       200:
+//  *         description: Added
+//  *       400:
+//  *         description: Bad Request
+//  *       404:
+//  *         description: Not Found
+//  */
+// router.route("/addPostContentImage/:blogPostId").put(adminProtect, protect, uploadMemoryStorage.array("Files[]"), async (req, res, next) => {
+//     try {
+//         if (req.files) {
+//             req.files.forEach((file) => {
+//                 file.originalname = "post-content_" + file.originalname + "_" + Date.now();
+//                 req.contentImageOriginalName = file.originalname;
+//                 uploadBytesResumable(ref(firebaseStorage, `attachments/${file.originalname}`), file.buffer, { contentType: file.mimetype});
+//             });
+//         }
+//
+//         setTimeout(() => {
+//             next();
+//         }, 5000);
+//     } catch (error) {
+//         next(error);
+//     }
+// }, addPostContentImage);
